e1b51fbaebb1ba6bdf5de61fbc70066fe897d2e3
Logan Devine <logan@zirco.dev>
2026-08-03 17:07:13 -0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "logandevine.com";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              zola
+              nixfmt
+            ];
+          };
+        }
+      );
+    };
+}
